254b88019e4514faaa245de24e38a87a70174f8f
AshS <emailash62@gmail.com>
2022-10-20 16:59:09 +0800

    added json, js and json-lock into folder,

---
+++ index.js
@@ -0,0 +1,10 @@
+const bootstrap = require('bootstrap')
+const jest = require('jest')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+const { appendFile } = require('fs')
+
+const path = require('path');
+
+app.listen(5000);
+

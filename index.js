d82b68b42e16f2d40549824a0b1d682debd2b737
AshS <emailash62@gmail.com>
2022-10-21 13:54:46 +0800

    added employee.js, manager.js files and linked them together. Built out index.js to include inquirer prompts with questions to ask.

--- index.js
+++ index.js
@@ -1,10 +1,89 @@
-const bootstrap = require('bootstrap')
+
 const jest = require('jest')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
 const { appendFile } = require('fs')
+const Manager = require('./lib/Manager')
+
 
 const path = require('path');
 
-app.listen(5000);
 
+console.log('Welcome to the team generator!')
+const teamMembers =[]
+const idArray=[]
+function appMenu(){
+    function createManager(){
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'managerName',
+                    message:"what is the manager's name?"
+                },
+                {
+                    type:'input',
+                    name: 'managerId',
+                    message: "what is the managers ID?",
+                },
+                {
+                    type:'input',
+                    name: 'managerEmail',
+                    message: "what is the managers Email?",
+                },
+                {
+                    type:'input',
+                    name: 'managerOfficeNumber',
+                    message: "what is the managers Office number?",
+                },
+            ])
+            .then((answers)=>{
+                const manager =new Manager(
+                    answers.managerName,
+                    answers.managerId,
+                    answers.managerEmail,
+                    answers.managerOfficeNumber
+                )
+                teamMembers.push(manager)
+                idArray.push(answers.managerId)
+                createTeam()
+            })
+    }
+
+    function createTeam(){
+        inquirer
+            .prompt([
+                {
+                    type:'list',
+                    name:'memberChoice',
+                    message:'Which role would you like to add?',
+                    choices:['Engineer','Intern','Nobody']
+                }
+            ])
+            .then((userChoices)=>{
+                switch(userChoices.memberChoice){
+                    case 'Engineer':
+                        addEngineer()
+                        break
+                    case 'Intern':
+                        addIntern()
+                        break
+                    default: 
+                        buildTeam()
+                }
+            })
+    }
+
+    function addEngineer(){
+
+    }
+    function addEngineer(){
+
+    }
+    function addEngineer(){
+
+    }
+}
+
+
+appMenu()
